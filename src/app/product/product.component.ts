e94ff4f9686c68ec29f493bae366b28f75672c9e
teja <tejavarma.developer@gmail.com>
2019-06-21 14:05:01 +0530

    observables

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,17 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductsService } from '../products.service';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
-
+export class ProductComponent implements OnInit, OnDestroy {
+  private looktime: Subscription;
   constructor(private product: ProductsService) { }
-
   ngOnInit() {
+    // let a = 1;
+    // setInterval(function(){ console.log(a++) },1000);
+    // this.looktime = interval(1000).subscribe((d) => { 
+    //   console.log(d); 
+    // });
+    // const testob = new Observable( observer => {
+    //   let a = 1;
+    //   setInterval( function(){
+    //     observer.next(a)
+    //     a++;
+    //   },1000)
+    // });
+    // this.looktime = testob.subscribe((d) => { 
+    //   console.log(d); 
+    // });
   }
+  ngOnDestroy(){
+    // this.looktime.unsubscribe();
+  }
+
   getProducts(){
     // console.log(this.product.getPros());
     return this.product.getPros();
